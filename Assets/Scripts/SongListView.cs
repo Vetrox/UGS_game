1600691c057a9557fe4870315e8df915040e9410
Vetrox <39677514+Vetrox@users.noreply.github.com>
2022-03-09 13:18:30 +0100

    Add basic level loading

--- Assets/Scripts/SongListView.cs
+++ Assets/Scripts/SongListView.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class SongListView : MonoBehaviour
 {
@@ -23,6 +25,7 @@ public class SongListView : MonoBehaviour
             string name = file.Substring(musicDir.Length);
             if (name.EndsWith(".json"))
             {
+                name = name.Substring(0, name.Length - ".json".Length);
                 print("music: " + name);
                 addLabel(name);
             } else
@@ -38,5 +41,18 @@ public class SongListView : MonoBehaviour
         item_go.GetComponentInChildren<Text>().text = text;
         item_go.transform.SetParent(m_ContentContainer);
         item_go.transform.localScale = Vector2.one;
+
+        item_go.GetComponent<Button>().onClick
+            .AddListener(new UnityAction(() => {
+                LoadLevel(text);
+            }));
+    }
+
+    void LoadLevel(string levelName)
+    {
+        print("Enter: SongListView::LoadLevel(" + levelName + ")");
+        GameManager.currentLevel = levelName;
+        SceneManager.LoadScene("Scene");
+        print("Exit: SongListView::LoadLevel(" + levelName + ")");
     }
 }
