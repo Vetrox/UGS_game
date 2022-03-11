f276630b7e2c884fb3400e5787fa85c6a075f55d
Vetrox <39677514+Vetrox@users.noreply.github.com>
2022-03-11 11:19:47 +0100

    improve jigglyness of camera and publizize variables

--- Assets/Scripts/CamPositionUpdater.cs
+++ Assets/Scripts/CamPositionUpdater.cs
@@ -6,13 +6,13 @@ public class CamPositionUpdater : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [Range(1f, 10f)]
-    [SerializeField] private float horizontalDistance = 1.6f;
+    public float horizontalDistance = 1.6f;
     [Range(1f, 10f)]
-    [SerializeField] public float verticalDistance = 1.6f;
+    public float verticalDistance = 1.6f;
     [Range(0.01f, 1f)]
-    [SerializeField] public float stiffnes = 0.75f;
+    public float stiffnes = 0.75f;
 
-    void LateUpdate()
+    void FixedUpdate()
     {
         Vector3 expected = player.position + Vector3.back * horizontalDistance + Vector3.up * verticalDistance;
         transform.position = Vector3.Lerp(transform.position, expected, stiffnes);
